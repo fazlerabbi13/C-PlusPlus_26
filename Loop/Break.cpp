3acc550d1588f590f7ae84a237930de030e1dda9
fazlerabbi13 <mohammad0132794@gmail.com>
2026-01-06 18:25:16 +0600

    break terminated inner and outer loop.

---
+++ Loop/Break.cpp
@@ -0,0 +1,22 @@
+// using break statement inside
+// nested for loop
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+    int sum = 0;
+
+    for (int i = 1; i <= 5; i++) {
+        if(i == 2)
+        break;
+        for (int j = 1; j <= 5; j++) {
+            if (i == 2)
+                break;
+            cout << "i = " << i << ", j = " << j << "\n";
+        }
+    }
+
+    return 0;
+}
