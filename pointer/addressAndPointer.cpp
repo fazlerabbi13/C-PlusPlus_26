5f59357dc15e357586aa6a18c8cc50fb7fb6cf2b
fazlerabbi13 <mohammad0132794@gmail.com>
2026-01-07 11:15:03 +0600

    find address of variable val1.

--- pointer/addressAndPointer.cpp
+++ pointer/addressAndPointer.cpp
@@ -3,6 +3,16 @@ using namespace std;
 
 int main(){
     int val1 = 5;
-    cout << &val1;
+    // cout << &val1;
+
+    // find address of variable val1
+    int *ptr = &val1;
+    cout << ptr;
+
+
+    // find value of variable val1
+    // int *ptr = &val1;
+    // cout << *ptr;
+
     return 0;
 }
