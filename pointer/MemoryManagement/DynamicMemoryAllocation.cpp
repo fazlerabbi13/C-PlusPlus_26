ace244e81b746f520de2ba6b5fd9418d1518b681
fazlerabbi13 <mohammad0132794@gmail.com>
2026-01-07 23:30:46 +0600

    deallocate the memory set pointers to nullptr.

--- pointer/MemoryManagement/DynamicMemoryAllocation.cpp
+++ pointer/MemoryManagement/DynamicMemoryAllocation.cpp
@@ -1,13 +1,16 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    int* age = new int{22};
-    float* height = new float{5.9};
+int main()
+{
+    int *age = new int{22};
+    float *height = new float{5.9};
 
     cout << *age << "\n";
     cout << *height << "\n";
 
+    delete age;
+    delete height;
 
     return 0;
 }
