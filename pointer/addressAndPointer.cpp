5e11514c00fe50a49b7fa9dc94429baa6bfb86bc
fazlerabbi13 <mohammad0132794@gmail.com>
2026-01-07 11:21:39 +0600

    Use separate scopes.

--- pointer/addressAndPointer.cpp
+++ pointer/addressAndPointer.cpp
@@ -1,18 +1,23 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int val1 = 5;
     // cout << &val1;
 
     // find address of variable val1
-    // int *ptr = &val1;
-    // cout << ptr;
+    {
+        int *ptr = &val1;
+        cout << ptr; // prints address
+    }
 
-
+    cout << "\n";
     // find value of variable val1
-    int *ptr = &val1;
-    cout << *ptr;
+    {
+        int *ptr = &val1;
+        cout << *ptr; // prints value
+    }
 
     return 0;
 }
