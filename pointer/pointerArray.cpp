1e0d6759af59898cd395041620879b63d4ecf218
fazlerabbi13 <mohammad0132794@gmail.com>
2026-01-07 15:51:45 +0600

    Displaying address using arrays.

--- pointer/pointerArray.cpp
+++ pointer/pointerArray.cpp
@@ -3,20 +3,37 @@ using namespace std;
 
 int main(){
     int arr[5];
-    int n;
-    cin >> n;
+    // int n;
+    // cin >> n;
 
-    cout << "Enter Element: ";
+    // cout << "Enter Element: ";
 
-    for(int i  = 0; i < n; i++){
-        cin >> *(arr + i);
+    // for(int i  = 0; i < n; i++){
+    //     cin >> *(arr + i);
+    // }
+
+    // cout << "Display element: ";
+
+    // for(int i  = 0; i < n; i++){
+    //     cout << *(arr + i) << "\n";
+    // }
+
+    int *ptr;
+    // Displaying address using arrays
+    for(int i = 0; i < 3; i++){
+        cout << &arr[i] << "\n";
     }
 
-    cout << "Display element: ";
+    cout << ".........." << "\n";
+    ptr = arr;
 
-    for(int i  = 0; i < n; i++){
-        cout << *(arr + i) << "\n";
+    // Displaying address using pointers
+    for(int i = 0; i < 3; i++){
+        cout << ptr + i << "\n";
     }
+
+    
+
 
     
     return 0;
