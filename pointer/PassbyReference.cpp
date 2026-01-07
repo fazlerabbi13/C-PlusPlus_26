0dced7c744486df1fa2b455a44ea862ac1614faf
fazlerabbi13 <mohammad0132794@gmail.com>
2026-01-07 16:24:58 +0600

    swap two number.

--- pointer/PassbyReference.cpp
+++ pointer/PassbyReference.cpp
@@ -9,6 +9,14 @@ void swap(int& n1, int& n2){
 }
 
 int main(){
+    int n1 = 5, n2 = 8;
+
+    cout << " Before swapping: " << "n1: " << n1 << " " << "n2: " << n2 << "\n";
+
+    swap(n1, n2);
+
+    cout << "After swapping: " << "n1: " <<  n1 << " " << "n2: " << n2 << "\n";
+
 
     return 0;
 }
