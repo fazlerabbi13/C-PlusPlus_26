72afa769fc2fdca1a80d9ed8ce6bd4ca60e657b5
fazlerabbi13 <mohammad0132794@gmail.com>
2026-01-08 12:03:26 +0600

    use pointer in structure.

--- Structure/PointerstoStructure.cpp
+++ Structure/PointerstoStructure.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
 using namespace std;
 
-struct Distance {
+struct Distance
+{
     int feet;
     float inch;
 };
 
-int main(){
+int main()
+{
     Distance d;
-    Distance* ptr = &d;
+    Distance *ptr = &d;
 
     cin >> (*ptr).feet;
     cin >> (*ptr).inch;
+
+    cout << (*ptr).feet << " feet " << (*ptr).inch << " inches\n";
     
     return 0;
 }
