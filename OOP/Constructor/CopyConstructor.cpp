113845f14ddee7c2c8c4adc6e900fccf433913a9
fazlerabbi13 <mohammad0132794@gmail.com>
2026-01-10 23:27:35 +0600

    define class and constructor.

---
+++ OOP/Constructor/CopyConstructor.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+class Field{
+    private:
+        double length;
+        double width;
+
+    public:
+        Field(double len, double wid)
+            :length{len},width{wid}{}
+};
+
+int main(){
+
+
+
+    return 0;
+}
