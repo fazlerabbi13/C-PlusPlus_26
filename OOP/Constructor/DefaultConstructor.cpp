2e4786c01aa021524d68b885726977d78ab61770
fazlerabbi13 <mohammad0132794@gmail.com>
2026-01-10 13:03:48 +0600

    create class name Field.

--- OOP/Constructor/DefaultConstructor.cpp
+++ OOP/Constructor/DefaultConstructor.cpp
@@ -1,12 +1,14 @@
 #include<iostream>
 using namespace std;
 
-class Wall {
+class Field {
     private:
         double length;
+        double width;
 
     public:
-        Wall
+        Wall()
+            :length{5.6}
 }
 
 int main(){
