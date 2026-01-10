c63ab65158b6c2d89a094d975c8135e07a41f93f
fazlerabbi13 <mohammad0132794@gmail.com>
2026-01-10 13:18:17 +0600

    create paramiterised constructor.

--- OOP/Constructor/DefaultConstructor.cpp
+++ OOP/Constructor/DefaultConstructor.cpp
@@ -7,11 +7,20 @@ class Field {
         double width;
 
     public:
-        Wall()
-            :length{5.6}
-}
+        Field( double Len, double wid)
+            : length{Len},width{wid}{
+
+            }
+        
+        // double calculate_area(){
+        //     return length * width;
+        // }
+};
 
 int main(){
+    // Field field1(10.5, 8.6);
+
+    // cout << "Area of Field 1: " << field1.calculate_area() << "\n";
 
     return 0;
 }
