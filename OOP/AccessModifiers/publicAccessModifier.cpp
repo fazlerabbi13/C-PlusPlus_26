ad69eed9695a2fce4f2d87eaa5808cc39ae82b00
fazlerabbi13 <mohammad0132794@gmail.com>
2026-01-13 21:21:16 +0600

    create class name Man with member variable and member function.

---
+++ OOP/AccessModifiers/publicAccessModifier.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+class Man{
+    public:
+        int age;
+
+    void displayAge(){
+        cout << age << "\n";
+    }
+}
+
+int main(){
+
+    return 0;
+}
