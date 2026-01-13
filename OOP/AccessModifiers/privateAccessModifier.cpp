b038a4bc9edba892031bd6e5feb04512e11c741d
fazlerabbi13 <mohammad0132794@gmail.com>
2026-01-13 21:36:07 +0600

    create class name Man and define member variable as private and member function in public and it access this variable.

---
+++ OOP/AccessModifiers/privateAccessModifier.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+class Man{
+    private:
+        int age;
+
+    public:
+        void displayAge(int a){
+            age = a;
+            cout << age << "\n";
+        }
+};
+int main(){
+    int age
+
+    return 0;
+}
