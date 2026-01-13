aa031418dd7627173e88966201f043ca9a7aaa34
fazlerabbi13 <mohammad0132794@gmail.com>
2026-01-13 21:53:57 +0600

    create object of Man1 class.

---
+++ OOP/AccessModifiers/protectedAccessModifier.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+class Man{
+    protected:
+        int age;
+};
+
+class Man1 : public Man {
+    public:
+        void displayAge(int a){
+            age = a;
+            cout << age << "\n";
+        }
+};
+int main(){
+
+    int age;
+
+    Man1 man1;
+
+    
+
+    return 0;
+}
