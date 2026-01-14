9172ea0bd9782c3d0fc8b912d77e65342187e98b
fazlerabbi13 <mohammad0132794@gmail.com>
2026-01-14 19:21:11 +0600

    declare array size and make display() function for showing array element.

---
+++ Array/passArrayIntoFunc.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+const int arra_size = 5;
+
+void display(int arr[]){
+    for(int i = 0; i < arra_size; i++){
+        cout << i + 1 << "\n";
+    }
+}
+
+int main(){
+
+    return 0;
+}
