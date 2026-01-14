b220975eda14088e3cf9b945d04261d7c8aed49a
fazlerabbi13 <mohammad0132794@gmail.com>
2026-01-14 19:27:04 +0600

    declare an array in main function and call display() function for this array.

--- Array/passArrayIntoFunc.cpp
+++ Array/passArrayIntoFunc.cpp
@@ -5,11 +5,13 @@ const int arra_size = 5;
 
 void display(int arr[]){
     for(int i = 0; i < arra_size; i++){
-        cout << i + 1 << "\n";
+        cout << i + 1 << " = " << arr[i] << "\n";
     }
 }
 
 int main(){
+    int arr[arra_size] = {5, 6, 9, 7, 8};
 
+    display(arr);
     return 0;
 }
