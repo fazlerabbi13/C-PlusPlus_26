22510b86067c7ff44f5d39aee265dfad0b465ff0
fazlerabbi13 <mohammad0132794@gmail.com>
2026-01-15 21:34:01 +0600

    take input using getline() function.

--- String/string.cpp
+++ String/string.cpp
@@ -5,12 +5,15 @@ using namespace std;
 
 int main(){
 
-    char str[100];
+    // char str[100];
+    string str1;
 
     // cin >> str;
-    cin.get(str, 100);
+    // cin.get(str, 100);
+    getline(cin, str1);
 
-    cout << str << "\n";
+
+    cout << str1 << "\n";
 
     return 0;
 }
