0cfd1e35c5940b0c985ab24dfd1db0591cd91864
fazlerabbi13 <mohammad0132794@gmail.com>
2026-01-16 21:01:47 +0600

    declare a string and find frist and last occurence.

--- String/string.cpp
+++ String/string.cpp
@@ -6,14 +6,30 @@ using namespace std;
 int main(){
 
     // char str[100];
-    string str1;
+    // string str1;
 
     // cin >> str;
     // cin.get(str, 100);
-    getline(cin, str1);
+    // getline(cin, str1);
 
 
-    cout << str1 << "\n";
+    // cout << str1 << "\n";
+
+    string str2 = "Hello world! This is noisy world.";
+
+    size_t frist_occurence = str2.find("world");
+
+    size_t last_occurrence = str2.rfind("world");
+
+    if(frist_occurence != string::npos){
+        cout << frist_occurence << "\n";
+        cout << last_occurrence << "\n";
+    }else{
+        cout << "'world' is not found." << "\n";
+    }
+
+
+
 
     return 0;
 }
