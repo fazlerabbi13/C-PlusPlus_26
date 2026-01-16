20bc57efeb63288fdda9786d197ed8d6aab2795e
fazlerabbi13 <mohammad0132794@gmail.com>
2026-01-16 21:49:15 +0600

    Compare Strings Alphabetically.

--- String/string.cpp
+++ String/string.cpp
@@ -27,19 +27,35 @@ int main()
     //     cout << "'world' is not found." << "\n";
     // }
 
-    string str3 = "Hello, World!";
 
-    cout << "before: " << str3 << "\n";
+    // some built in function in string 
+    // string str3 = "Hello, World!";
 
-    str3.append("This is Fazle Rabbi ");
+    // cout << "before: " << str3 << "\n";
+
+    // str3.append("This is Fazle Rabbi ");
 
     // str3.insert(6, " of programming ");
 
     // str3.erase(5, 9);
 
-    str3.replace(7, 3, "Programmer");
+    // str3.replace(7, 3, "Programmer");
 
-    cout << "after: " << str3 << "\n";
+    // cout << "after: " << str3 << "\n";
+
+    // Compare Strings Alphabetically
+
+    string str4 = "Hello";
+    string str7 = "Hello";
+    string str5 = "Programmers World";
+    string str6 = "c++ Programming";
+
+
+    cout << str4.compare(str5) << "\n";
+    cout << str4.compare(str7) << "\n";
+    cout << str5.compare(str4) << "\n";
+
+
 
     return 0;
 }
