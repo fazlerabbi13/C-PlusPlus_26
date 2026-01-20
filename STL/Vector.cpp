d67699d9db2743d6dd391f934d5f75f821266ed9
fazlerabbi13 <mohammad0132794@gmail.com>
2026-01-20 22:46:48 +0600

    increase vector size using resize() function.

--- STL/Vector.cpp
+++ STL/Vector.cpp
@@ -7,17 +7,18 @@ int main(){
     // range constructor vector to vector
     vector<int> v1(5);
     
-    for(int i = 0; i < v1.size(); i++)
-    cin >> v1[i];
+    // for(int i = 0; i < v1.size(); i++)
+    // cin >> v1[i];
     
-    vector<int> v2(v1.rbegin(), v1.rend());
+    // vector<int> v2(v1.rbegin(), v1.rend());
 
-    for(int i = 0; i <v2.size(); i++)
-    cout << v2[i] <<"\n";
+    // for(int i = 0; i <v2.size(); i++)
+    // cout << v2[i] <<"\n";
 
 
 
-    // int arr[] = {102, 7, 5};
-    // v.assign(arr, arr + 4);
-    // cout << "size of vector: " << int(v.size()) << " value at index 2: " << v.at(2) << "\n";
+    int arr[] = {102, 7, 5};
+    v1.assign(arr, arr + 4);
+    v1.resize(10);
+    cout << "size of vector: " << int(v1.size()) << " value at index 2: " << v1.at(2) << "\n";
 }
